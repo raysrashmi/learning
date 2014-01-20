cf6c9e24b99c41f2b02a444172df314e68658487
Rashmi Yadav <rays.rashmi@gmail.com>
2014-01-20 22:13:18 +0100

    Updated

--- proc_lambda.rb
+++ proc_lambda.rb
@@ -7,3 +7,14 @@ my_lambda = lambda do|a|
   puts "number is #{a}"
 end
 my_lambda.call(10 )
+
+#DIFFFERENCE
+my_proc = proc do|a|
+  puts "this is proc"
+end
+p my_proc.call #this is proc
+
+my_lambda =lambda do |a|
+  puts "this is lambda"
+end
+p my_lambda.call 
